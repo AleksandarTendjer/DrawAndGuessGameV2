5b41cfeccbeaed73a4cd1b41d3cc877c60a25339
AleksandarTendjer <tendjer@protonmail.com>
2020-01-10 21:30:30 +0100

    added registration and login(have to add chat tonight)

---
+++ server.js
@@ -0,0 +1,215 @@
+/*jshint esversion: 6 */
+
+let startTime = Date.now();
+
+// Dependencies
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+var ejs=require('ejs');
+var mongoose=require('mongoose');
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+const port = process.env.PORT || 3000;
+var dbo=require('./dbOperations');
+var game = require("./controller");
+
+var bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_URI || "mongodb://heroku_nft10gvf:822nen9r3b8inasjmj2bg8ks0h@ds049548.mlab.com:49548/heroku_nft10gvf";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let collection;
+
+client.connect(err => {
+    if (err) return console.error(err);
+    collection = client.db('heroku_nft10gvf').collection("words");
+//connecting to the database
+// connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/DrawAndGuessGame')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+    // Starts the server.
+    server.listen(port, function() {
+        console.log('Server started on port ' + port);
+    });
+});
+
+// number of words in database
+let totalWords = 10;
+
+app.set('port', port);
+app.use('/static', express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routing
+app.get('/', function(request, res) {
+  //try to get login in index
+    res.render(path.join(__dirname + '/static', 'index'));
+});
+//app.post('/login/',function(req, res){
+  //the data comming from the form
+  //console.log("request");
+  //console.log(req.body);
+app.post("/login/",game.login);
+app.post('/register/',game.join);
+
+let lobbies = [];
+
+let Lobby = function() {
+    let lobbyNum = lobbies.length;
+    this.lobbyId = "lobby" + lobbyNum.toString();
+    this.players = [];
+    this.lastDataUrl = null;
+    this.drawingPlayer = null;
+    this.word = null;
+    this.timer = null;
+    this.timeLeft = null;
+    this.guessedPlayers = [];
+};
+
+let playerLobbies = {};
+
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+    socket.on('newPlayer', function(username) {
+        if (lobbies.length == 0) {
+            lobbies.push(new Lobby());
+        } else if (lobbies[0].players.length == 6) {
+            lobbies.splice(0, 0, new Lobby());
+        }
+        socket.join(lobbies[0].lobbyId);
+
+        console.log(username + " connected!");
+        lobbies[0].players.push({ id: socket.id, username: username, score: 0 });
+        io.in(lobbies[0].lobbyId).emit('joinSound');
+        playerLobbies[socket.id] = lobbies[0];
+
+        if (lobbies[0].players.length == 1) {
+            lobbies[0].drawingPlayer = socket.id;
+            let rnd = Math.floor(Math.random() * totalWords);
+            collection.findOne({ _id: rnd }, (err, res) => {
+                if (err) return console.error(err);
+                io.to(lobbies[0].drawingPlayer).emit('letsDraw', res.word);
+                lobbies[0].word = res.word;
+                lobbies[0].guessedPlayers = [];
+            });
+            io.in(lobbies[0].lobbyId).emit('waiting');
+        } else {
+            if (lobbies[0].players.length == 2) {
+                next_turn(lobbies[0]);
+            }
+            io.in(lobbies[0].lobbyId).emit('letsWatch', lobbies[0].drawingPlayer, lobbies[0].lastDataUrl);
+            io.to(socket.id).emit('makeaguess', lobbies[0].drawingPlayer);
+        }
+        io.in(lobbies[0].lobbyId).emit('updateSB', lobbies[0].players, lobbies[0].drawingPlayer);
+    });
+
+    socket.on('view', function(dataURL) {
+        let currLobby = playerLobbies[socket.id];
+        if (currLobby) {
+            currLobby.lastDataUrl = dataURL;
+            io.in(currLobby.lobbyId).emit('letsWatch', socket.id, dataURL);
+        }
+    });
+
+    socket.on('guess', function(word) {
+        let currLobby = playerLobbies[socket.id];
+        if (currLobby && socket.id != currLobby.drawingPlayer && word == currLobby.word && !currLobby.guessedPlayers.includes(socket.id)) {
+            io.to(socket.id).emit('guessRes', "CORRECT!");
+            let i = currLobby.players.map(function(e) { return e.id; }).indexOf(socket.id);
+            currLobby.players[i].score += (60 - Math.ceil((Date.now() - startTime - currLobby.timer._idleStart) / 1000)) + 1;
+            let j = currLobby.players.map(function(e) { return e.id; }).indexOf(currLobby.drawingPlayer);
+            currLobby.players[j].score += 20;
+            io.in(currLobby.lobbyId).emit('updateSB', currLobby.players, currLobby.drawingPlayer);
+            currLobby.guessedPlayers.push(socket.id);
+        } else if (currLobby && socket.id != currLobby.drawingPlayer && word != currLobby.word && !currLobby.guessedPlayers.includes(socket.id)) {
+            io.to(socket.id).emit('guessRes', "TRY AGAIN!");
+        }
+    });
+
+    socket.on('disconnect', function() {
+        let currLobby = playerLobbies[socket.id];
+        if (!currLobby) return;
+        let i = currLobby.players.map(function(e) { return e.id; }).indexOf(socket.id);
+        console.log(currLobby.players[i].username + " disconnected!");
+        currLobby.players.splice(i, 1);
+        io.in(currLobby.lobbyId).emit('disconnectSound');
+
+        if (socket.id == currLobby.drawingPlayer) {
+            currLobby.lastDataUrl = null;
+            if (currLobby.players.length > 0) {
+                clearInterval(currLobby.timer);
+                clearInterval(currLobby.timeLeft);
+                if (i < currLobby.players.length)
+                    currLobby.drawingPlayer = currLobby.players[i].id;
+                else
+                    currLobby.drawingPlayer = currLobby.players[0].id;
+                let rnd = Math.floor(Math.random() * totalWords);
+                collection.findOne({ _id: rnd }, (err, res) => {
+                    if (err) return console.error(err);
+                    io.to(currLobby.drawingPlayer).emit('letsDraw', res.word);
+                    currLobby.word = res.word;
+                    currLobby.guessedPlayers = [];
+                });
+                if (currLobby.players.length > 1) {
+                    io.in(currLobby.lobbyId).emit('letsWatch', currLobby.drawingPlayer, currLobby.lastDataUrl);
+                    io.in(currLobby.lobbyId).emit('makeaguess', currLobby.drawingPlayer);
+                    next_turn(currLobby);
+                    io.in(currLobby.lobbyId).emit('timer', "60");
+                } else {
+                    io.in(currLobby.lobbyId).emit('waiting');
+                    let j = lobbies.map(function(e) { return e.lobbyId; }).indexOf(currLobby.lobbyId);
+                    lobbies.splice(j, i);
+                    lobbies.splice(0, 0, currLobby);
+                }
+            } else {
+                lobbies = lobbies.filter(function(lobby) {
+                    return lobby.lobbyId != currLobby.lobbyId;
+                });
+            }
+        }
+
+        if (currLobby.players.length < 2) {
+            clearInterval(currLobby.timer);
+            clearInterval(currLobby.timeLeft);
+            io.in(currLobby.lobbyId).emit('waiting');
+        }
+
+        io.in(currLobby.lobbyId).emit('updateSB', currLobby.players, currLobby.drawingPlayer);
+    });
+});
+
+function next_turn(lobby) {
+    lobby.timer = setInterval(function() {
+        let i = lobby.players.map(function(e) { return e.id; }).indexOf(lobby.drawingPlayer);
+        lobby.lastDataUrl = null;
+        if (i < lobby.players.length - 1) {
+            lobby.drawingPlayer = lobby.players[i + 1].id;
+        } else {
+            lobby.drawingPlayer = lobby.players[0].id;
+        }
+        let rnd = Math.floor(Math.random() * totalWords);
+        collection.findOne({ _id: rnd }, (err, res) => {
+            if (err) return console.error(err);
+            io.to(lobby.drawingPlayer).emit('letsDraw', res.word);
+            lobby.word = res.word;
+            lobby.guessedPlayers = [];
+        });
+        io.in(lobby.lobbyId).emit('letsWatch', lobby.drawingPlayer, lobby.lastDataUrl);
+        io.in(lobby.lobbyId).emit('makeaguess', lobby.drawingPlayer);
+        io.in(lobby.lobbyId).emit('updateSB', lobby.players, lobby.drawingPlayer);
+        io.in(lobby.lobbyId).emit('nextTurn');
+    }, 60000);
+
+    lobby.timeLeft = setInterval(function() {
+        let timeleft = (60 - Math.ceil((Date.now() - startTime - lobby.timer._idleStart) / 1000));
+        io.in(lobby.lobbyId).emit('timer', timeleft.toString());
+    }, 1000);
+}
---
+++ static/game.js
@@ -0,0 +1,392 @@
+/*jshint esversion: 6 */
+
+(function() {
+    let socket = io();
+    "use strict"; // jshint ignore:line
+    //https://www.reddit.com/r/discordapp/comments/4ytdf0/where_are_sound_files_stored/
+    let joinSound = new Audio('static/sfx/joinSound.mp3');
+    let disconnectSound = new Audio('static/sfx/disconnectSound.mp3');
+    //http://soundbible.com/tags-tick.html
+    let tick = new Audio('static/sfx/tick.mp3');
+    //https://www.freesoundeffects.com/cat/correct-292/
+    let correct = new Audio('static/sfx/correct.mp3');
+    //https://www.youtube.com/watch?v=4k8XfsqkU3o
+    let nextTurn = new Audio('static/sfx/nextTurn.mp3');
+
+    let alphanumeric = /^[0-9a-zA-Z]+$/;
+
+    let msgID = document.getElementById('msgID');
+    msgID.addEventListener('keyup', function onEvent(e) {
+        if (e.keyCode === 13) {
+            if (msgID.value.match(alphanumeric)) {
+                msgID.placeholder = "What is your guess?";
+
+                socket.emit("guess", msgID.value.toLowerCase());
+                msgID.value = "";
+            } else {
+                msgID.placeholder = "invalid input";
+                msgID.value = "";
+            }
+        }
+    });
+    //hiding elements on login
+      document.getElementById("canvasDraw").style.display = "none";
+    document.getElementById("canvasView").style.display = "none";
+    hideClass(document.getElementsByClassName("utils"));
+    hideClass(document.getElementsByClassName("msg"));
+
+    hideClass(document.getElementsByClassName("word"));
+    hideClass(document.getElementsByClassName("info"));
+    hideClass(document.getElementsByClassName("drawing"));
+
+    //have to create the login logic here(when loggedIn)
+    let usernameID = document.getElementById('usernameID');
+
+    var regisertForm= document.getElementById('registerForm');
+    var loginForm= document.getElementById('loginForm');
+
+
+
+function registrationFun(data)
+{
+  var username=data.username.value;
+  var password=data.password.value;
+
+//add user
+  $.post({
+      type: 'POST',
+      url: 'http://localhost:3000/regster',
+    //  contentType: "application/json",
+  data: {username:username,password:password},//JSON.stringify(Status),
+    //dataType: "json",
+      //$.toJSON({ sendData: dataPackage })
+      success: function(data){
+        //redirect to login
+        debugger;
+          alert("The user has been succesfully created! ");
+
+          window.location.assign('http://localhost:3000/');
+        //change the value of the field that was changed
+        //  location.reload();
+    },          error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    debugger;
+                    alert("Status: " + textStatus); alert("Error: " + errorThrown);
+                }
+
+  });
+
+}
+function loggedInFunction(data) {
+//  usernameID.addEventListener('keyup', function onEvent(e) {
+  //    if (e.keyCode === 13) {
+    //      if (usernameID.value.match(alphanumeric)) {
+
+              document.getElementsByClassName("login-section")[0].style.display = "none";
+              showClass(document.getElementsByClassName("utils"));
+              //this is where we emit the new player
+
+              socket.emit('newPlayer',data.username/* usernameID.value*/);
+              showClass(document.getElementsByClassName("word"));
+              showClass(document.getElementsByClassName("info"));
+              showClass(document.getElementsByClassName("drawing"));
+
+          }
+
+
+
+function loginFun(username,password)
+{
+  //event.preventDefault();
+	debugger;
+	//username or password(mainly username for now )
+
+//add user
+  $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/login/',
+    //  contentType: "application/json",
+  data: {username:username,password:password},
+  success:function(data){
+    debugger;
+  loggedInFunction(data);
+},
+error: function()
+{
+  alert("incorrect password");
+}
+			//JSON.stringify(Status),
+});
+}
+
+loginForm.addEventListener("submit",function(e){
+debugger;
+e.preventDefault();
+  loginFun(this.email.value,this.password.value)
+});
+
+    function hideClass(cls) {
+        for (let elem of cls) {
+            elem.style.display = "none";
+        }
+    }
+
+    function showClass(cls) {
+        for (let elem of cls) {
+            elem.style.display = "initial";
+        }
+    }
+
+    socket.on('joinSound', function() {
+        joinSound.play();
+    });
+
+    socket.on('disconnectSound', function() {
+        disconnectSound.play();
+    });
+
+    socket.on('nextTurn', function() {
+        nextTurn.play();
+    });
+
+    socket.on('updateSB', function(players, drawingPlayer) {
+        let turns = document.getElementById('turnsID');
+        turns.innerHTML = '';
+        for (let player of players) {
+            let div = document.createElement('div');
+            div.className = 'playerCard';
+            if (player.id == drawingPlayer) {
+                div.id = 'drawingPlayerCard';
+            }
+            div.innerHTML =
+                '<div class="scoreBoard">' +
+                '<div>' + player.username + '</div>' +
+                '<div>' + player.score + '</div>' +
+                '</div>' +
+                '<div class="arrowRight"></div>';
+            turns.appendChild(div);
+        }
+
+        //end card
+        let end = document.createElement('div');
+        end.className = 'endOfRound';
+        end.innerHTML = '<div>END OF ROUND!</div>';
+        turns.appendChild(end);
+    });
+
+    socket.on('guessRes', function(res) {
+        let wordElem = document.getElementById('wordID');
+        wordElem.textContent = res;
+        if (res == "CORRECT!")
+            correct.play();
+    });
+
+    socket.on('timer', function(timeleft) {
+        infoElem = document.getElementById('infoID');
+        infoElem.setAttribute('style', 'white-space: pre;');
+        infoElem.textContent = timeleft + "\r\nSECONDS\r\nREMAINING!";
+        if (parseInt(timeleft) <= 5) {
+            tick.play();
+        }
+    });
+
+    socket.on('waiting', function() {
+        infoElem = document.getElementById('infoID');
+        infoElem.setAttribute('style', 'white-space: pre;');
+        infoElem.textContent = "WAITING FOR\r\nADDITIONAL\r\nPLAYERS...";
+    });
+
+    socket.on('letsWatch', function(leaderSocket, dataURL) {
+        if (socket.id != leaderSocket) {
+            document.getElementById("canvasDraw").style.display = "none";
+            document.getElementById("canvasView").style.display = "initial";
+            hideClass(document.getElementsByClassName("utils"));
+            showClass(document.getElementsByClassName("msg"));
+
+            let context = document.getElementById("canvasView").getContext("2d");
+            let image = new Image();
+            image.onload = function() {
+                context.clearRect(0, 0, canvasView.width, canvasView.height);
+                context.drawImage(image, 0, 0);
+            };
+            if (dataURL)
+                image.src = dataURL;
+            else {
+                let context = document.getElementById("canvasView").getContext("2d");
+                context.clearRect(0, 0, canvasDraw.width, canvasDraw.height);
+            }
+        }
+    });
+
+    socket.on('makeaguess', function(leaderSocket) {
+        if (socket.id != leaderSocket) {
+            let wordElem = document.getElementById('wordID');
+            wordElem.textContent = "Make a guess...";
+        }
+    });
+
+    socket.on('letsDraw', function(word) {
+        document.getElementById("canvasDraw").style.display = "initial";
+        document.getElementById("canvasView").style.display = "none";
+        showClass(document.getElementsByClassName("utils"));
+        hideClass(document.getElementsByClassName("msg"));
+
+        let wordElem = document.getElementById('wordID');
+        wordElem.textContent = "It's your turn to draw: " + word.toUpperCase();
+
+        let context = document.getElementById("canvasDraw").getContext("2d");
+
+        // canvas drawing functions base code from
+        // http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/ and
+        // https://github.com/ThierrySans/CSCC09/blob/master/lectures/02/src/html5/js/draw.js
+        let clickX = [];
+        let clickY = [];
+        let clickDrag = [];
+        let clickColor = [];
+        let clickSize = [];
+        let paint = false;
+        let currentColor = "#000000";
+        let currentSize = 4;
+
+        let prepareCanvas = function() {
+            clearCanvas();
+
+            canvasDraw.onmousedown = function(e) {
+                let rect = canvasDraw.getBoundingClientRect();
+                let mouseX = e.pageX - this.offsetLeft;
+                let mouseY = e.pageY - this.offsetTop;
+
+                let scaleX = canvasDraw.width / rect.width;
+                let scaleY = canvasDraw.height / rect.height;
+
+                paint = true;
+                addClick(mouseX * scaleX, mouseY * scaleY, false);
+                redraw();
+            };
+
+            canvasDraw.onmousemove = function(e) {
+                if (paint) {
+                    let rect = canvasDraw.getBoundingClientRect();
+                    let mouseX = e.pageX - this.offsetLeft;
+                    let mouseY = e.pageY - this.offsetTop;
+
+                    let scaleX = canvasDraw.width / rect.width;
+                    let scaleY = canvasDraw.height / rect.height;
+
+                    addClick(mouseX * scaleX, mouseY * scaleY, true);
+                    redraw();
+                }
+            };
+
+            canvasDraw.onmouseup = function(e) {
+                paint = false;
+            };
+
+            canvasDraw.onmouseleave = function(e) {
+                paint = false;
+            };
+        };
+
+        function addClick(x, y, dragging) {
+            clickX.push(x);
+            clickY.push(y);
+            clickDrag.push(dragging);
+            clickColor.push(currentColor);
+            clickSize.push(currentSize);
+        }
+
+        let clearCanvas = function() {
+            context.clearRect(0, 0, canvasDraw.width, canvasDraw.height);
+        };
+
+        let redraw = function() {
+            clearCanvas();
+            context.lineJoin = "round";
+            for (let i = 0; i < clickX.length; i++) {
+                if (clickX[i] == -1)
+                    clearCanvas();
+                else {
+                    context.beginPath();
+                    if (clickDrag[i] && i)
+                        context.moveTo(clickX[i - 1], clickY[i - 1]);
+                    else
+                        context.moveTo(clickX[i] - 1, clickY[i]);
+                    context.lineTo(clickX[i], clickY[i]);
+                    context.closePath();
+                    context.strokeStyle = clickColor[i];
+                    context.lineWidth = clickSize[i];
+                    context.stroke();
+                }
+            }
+            let dataURL = canvasDraw.toDataURL();
+            socket.emit('view', dataURL);
+        };
+
+        let resetCanvas = function() {
+            if (clickX[clickX.length - 1] == -1)
+                return;
+            clickX.push(-1);
+            clickY.push(null);
+            clickDrag.push(null);
+            clickColor.push(null);
+            clickSize.push(null);
+            redraw();
+        };
+
+        let undoLast = function() {
+            clickX.pop();
+            clickY.pop();
+            clickDrag.pop();
+            clickColor.pop();
+            clickSize.pop();
+            redraw();
+        };
+
+        let changeColor = function() {
+            currentColor = this.value;
+        };
+
+        let setSizeSmall = function() {
+            currentSize = 2;
+        };
+
+        let setSizeRegular = function() {
+            currentSize = 4;
+        };
+
+        let setSizeBig = function() {
+            currentSize = 8;
+        };
+
+        // function base code from
+        // https://stackoverflow.com/questions/79816/need-javascript-code-for-button-press-and-hold
+        function heldDown(btn, action, initial, start = initial) {
+            let t;
+
+            let repeat = function() {
+                action();
+                t = setTimeout(repeat, start);
+                if (start > 8)
+                    start = start / 2;
+            };
+
+            btn.onmousedown = function() {
+                repeat();
+            };
+
+            btn.onmouseup = function() {
+                clearTimeout(t);
+                start = initial;
+            };
+
+            btn.onmouseleave = btn.onmouseup;
+        }
+
+        prepareCanvas();
+        document.getElementById("clearID").onclick = resetCanvas;
+        heldDown(document.getElementById("undoID"), undoLast, 250);
+        document.getElementById("colorID").oninput = changeColor;
+        document.getElementById("smallID").onclick = setSizeSmall;
+        document.getElementById("mediumID").onclick = setSizeRegular;
+        document.getElementById("largeID").onclick = setSizeBig;
+    });
+})();
